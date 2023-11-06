37548b31e05e5568eb254a089f35ed66f4385aab
Jonas Faber <jonas.h.faber@gmail.com>
2023-11-06 10:30:48 +0100

    remove log

--- server/navigation/MobileNavToggle.tsx
+++ server/navigation/MobileNavToggle.tsx
@@ -6,7 +6,6 @@ interface Props {
 }
 
 export const MobileNavToggle = ({ open, ...otherProps }: Props) => {
-  console.log(otherProps);
   return (
     <button
       className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
