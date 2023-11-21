17fd128919ce6fda23a9241be013f85a962ce632
Joren-vanGoethem <jorenvangoethem@hotmail.com>
2023-11-21 19:03:28 +0100

    hello there attempt

--- client/ThemeSwitchers/ThemeSelector.tsx
+++ client/ThemeSwitchers/ThemeSelector.tsx
@@ -1,11 +1,11 @@
 "use client";
-import { useEffect, useState } from "react";
-import { useTheme } from "next-themes";
+import {useEffect, useState} from "react";
+import {useTheme} from "next-themes";
 import "./ThemeSelector.css";
 
 export const ThemeSelector = () => {
   const [mounted, setMounted] = useState(false);
-  const { systemTheme, theme, setTheme } = useTheme();
+  const {systemTheme, theme, setTheme} = useTheme();
 
   // useEffect only runs on the client, so now we can safely show the UI
   useEffect(() => {
--- providers/ThemeProvider.tsx
+++ providers/ThemeProvider.tsx
@@ -1,8 +1,8 @@
 "use client";
 import * as React from "react";
-import { ReactNode } from "react";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
+import {ReactNode} from "react";
+import {ThemeProvider as NextThemesProvider} from "next-themes";
 
-export function ThemeProvider({ children, ...props }: { children: ReactNode }) {
+export function ThemeProvider({children, ...props}: { children: ReactNode }) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
