9f71e7991f5a83c3d67ba20c0a822eb0dd64bc1d
Joren-vanGoethem <jorenvangoethem@hotmail.com>
2023-12-10 13:00:07 +0100

    add classname

--- src/components/atoms/button/Button.tsx
+++ src/components/atoms/button/Button.tsx
@@ -3,8 +3,10 @@ import { twMerge } from 'tailwind-merge'
 import {WithChildren} from "@/interfaces/WithChildren";
 import styles from "./Button.module.scss"
 import {useButton, AriaButtonOptions} from 'react-aria';
+import {WithClick} from "@/interfaces/WithClick";
+import {WithClassName} from "@/interfaces/WithClassname";
 
-interface ButtonProps extends AriaButtonOptions<'button'>, WithChildren {
+interface ButtonProps extends AriaButtonOptions<'button'>, WithChildren, WithClassName {
   variant?: Variant;
 }
 
@@ -12,7 +14,7 @@ export const Button = (props: ButtonProps) => {
   let ref = React.useRef(null);
   let { buttonProps } = useButton(props, ref);
 
-  buttonProps.className = twMerge(styles[props.variant ?? "unstyled"], buttonProps.className)
+  buttonProps.className = twMerge(styles[props.variant ?? "unstyled"], buttonProps.className, props.className)
 
   return (
     <button
